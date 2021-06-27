212839075227a7893ef0c87a702dad7ea0216bf0
Lavan Sumanan <57576109+LavanSumanan@users.noreply.github.com>
2021-06-27 08:10:23 -0400

    Delete arduino_secrets.h

--- PeeSensor/arduino_secrets.h
+++
@@ -1,4 +0,0 @@
-#define SECRET_HOST "dogtime-1f1d5-default-rtdb.firebaseio.com"
-#define SECRET_AUTH "FAagzVcDktIW8ns3cWcWndqDg1rWeAwap2jDeEjP"
-#define SECRET_SSID "Police_Scanner"
-#define SECRET_PASS "Luxlav01"
